b3666f116d661a9b1ce74373eb6e976d15beff06
vartanyanE <mike.vartanyan@yahoo.com>
2020-12-10 20:23:50 -0800

    added mvc

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
- import express from 'express';
-import {getPosts} from '../controllers/posts.js'
-import {createPost} from '../controllers/posts.js'
- const router = express.Router();
+import express from "express";
+import { getPosts } from "../controllers/posts.js";
+import { createPost } from "../controllers/posts.js";
+const router = express.Router();
 
- router.get('/', getPosts );
- router.post('/', createPost);
+router.get("/", getPosts);
+router.post("/", createPost);
 
- export default router;
+export default router;
