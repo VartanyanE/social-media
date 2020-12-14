2d6a8dfe4943c700de084d10bbd8da213e661962
VartanyanE <mike.vartanyan@yahoo.com>
2020-12-13 21:45:55 -0800

    added patch route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
+
 
 export const getPosts = async (req, res) => {
   try {
@@ -25,3 +27,16 @@ export const createPost = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
 };
+
+export const updatePost = async (req, res) => {
+  const {id: _id} = req.params;
+  const post = req.body;
+
+  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new:true});
+
+  res.json(updatedPost)
+
+
+}
